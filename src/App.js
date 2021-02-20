1a182391ccf57f9d29b7f1cb54dfd442bd454af5
Rohit <varsadarohit@gmail.com>
2021-02-20 16:41:57 +0530

    Complete

--- src/App.js
+++ src/App.js
@@ -1,45 +1,70 @@
 import React, { useEffect } from "react";
 import { PDFViewer } from "@react-pdf/renderer";
-import tiles1 from "./iii/24support.png";
-import TextField from "@material-ui/core/TextField";
 import Modal from "@material-ui/core/Modal";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from "@material-ui/icons/Delete";
-import {
-  Page,
-  Text,
-  View,
-  Document,
-  StyleSheet,
-  Image,
-} from "@react-pdf/renderer";
+import Bill from "./Components/Bill";
 import { useState } from "react";
 import RowsInput from "./Components/RowsInput";
 import TableHead from "./Components/TableHead";
 import "./App.css";
+import Form from "./Components/Form";
 
 function App() {
-  const [items, setItems] = useState([{ name: "Rohit", rate: 0, quantity: 0 }]);
-  const [itemNo, setItemNo] = useState(1);
+  const [items, setItems] = useState([]);
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
   const [labour, setLabour] = useState(0);
+  const [image, setImage] = useState();
+  const [showBill, setShowBill] = useState(false);
 
   useEffect(() => {
     console.log("yuppp");
     return () => {};
   }, [items]);
 
+  function clearAllBillDetail() {
+    setItems([]);
+    setName("");
+    setDate("");
+    setLabour(0);
+    setImage("");
+  }
+
   function ondelete(ind) {
-    let temp = items.filter(function (val, index, arr) {
+    let temp = items.filter(function (val, index) {
       return index != ind;
     });
     setItems(temp);
-    console.log(items);
+  }
+
+  function onLabourChange(value) {
+    setLabour(value);
+  }
+
+  function onImageChange(value) {
+    setImage(value);
+  }
+
+  function onNameChange(value) {
+    setName(value);
+  }
+
+  function onDateChange(value) {
+    setDate(value);
+  }
+
+  function onAddItem(obj) {
+    setItems([
+      ...items,
+      { name: obj.name, rate: obj.rate, quantity: obj.quantity },
+    ]);
   }
 
   const getTotal = () => {
     var total = 0;
-    items.map(function (x, i) {
+    items.map(function (x) {
       total = total + x.quantity * x.rate;
     });
     return total + labour;
@@ -47,107 +72,147 @@ function App() {
 
   return (
     <div
-      className="container my-5"
+      className="container-fluid my-5"
       style={{ display: "flex", justifyContent: "center" }}
     >
+      <div className="col-6">
+        <Form
+          onNameChange={onNameChange}
+          onDateChange={onDateChange}
+          onAddItem={onAddItem}
+          onLabourChange={onLabourChange}
+          onImageChange={onImageChange}
+          image={image}
+          labour={labour}
+          n={name}
+          date={date}
+        />
+      </div>
+
       <div
-        className="px-5"
-        style={{
-          border: "1px solid black",
-          borderRadius: 10,
-          boxShadow: "5px 5px 20px 3px #bfbfbf",
-          width: "fit-content",
-          padding: 15,
-        }}
+        className="col-6"
+        style={{ display: "flex", justifyContent: "center" }}
       >
-        <h3 className="text-center my-3">DETAIL BILL</h3>
-        <div className="row mb-3">
-          <div className="col-6 row">
-            <div className="col-3">Name :</div>
-            <TextField type="text" className="col-9" />
-          </div>
-          <div
-            className="col-6"
-            style={{ justifyContent: "flex-end", display: "flex" }}
-          >
-            <TextField type="date" />
-          </div>
-        </div>
-        <table>
-          <TableHead />
-          {items.map(function (item, i) {
-            return (
-              <tr>
-                <RowsInput
-                  items={items}
-                  setItems={setItems}
-                  index={i}
-                  qa={item.quantity}
-                  ra={item.rate}
-                  na={item.name}
-                  ondelete={ondelete}
-                />
-                {/* <p>{item.name}</p> */}
-                <td style={{ border: 0 }}>
-                  <IconButton
-                    style={{ padding: 0 }}
-                    aria-label="delete"
-                    onClick={() => {
-                      ondelete(i);
-                    }}
-                  >
-                    <DeleteIcon fontSize="small" />
-                  </IconButton>
-                </td>
-              </tr>
-            );
-          })}
-          <tr>
-            <td colSpan={3} className="labour text-center">
-              Labour
-            </td>
-            <td>
-              <TextField
-                inputProps={{ min: 0, style: { textAlign: "center" } }}
-                placeholder={0}
-                type="number"
-                fullWidth
-                onChange={(event) => setLabour(parseInt(event.target.value))}
-              />
-            </td>
-          </tr>
-          <tr>
-            <th colSpan={3} className="text-center">
-              Total
-            </th>
-            <th className="total">{getTotal()}</th>
-          </tr>
-        </table>
-        <Button
-          className="my-3"
-          onClick={() =>
-            setItems([...items, { name: "", rate: 0, quantity: 0 }])
-          }
-          color="primary"
-          variant="contained"
+        <div
+          className="px-5 scroll"
+          style={{
+            border: "1px solid black",
+            borderRadius: 10,
+            boxShadow: "5px 5px 20px 3px #bfbfbf",
+            width: "90%",
+            height: "85vh",
+
+            overflow: "auto",
+            padding: 15,
+          }}
         >
-          Add Item
-        </Button>
-        <tr
+          <h3 className="text-center my-3">DETAIL BILL</h3>
+          <div className="row mb-3">
+            <div className="col-6 row">Name : {name}</div>
+            <div
+              className="col-6"
+              style={{ justifyContent: "flex-end", display: "flex" }}
+            >
+              Date : {date}
+            </div>
+          </div>
+          <div className="text-center">
+            {image && (
+              <img
+                className="my-3"
+                src={image}
+                alt="new"
+                height={100}
+                width={100}
+              />
+            )}
+          </div>
+          <table>
+            <TableHead />
+            {items.map(function (item, i) {
+              console.log(item);
+              return (
+                <tr>
+                  <RowsInput index={i} item={item} />
+
+                  <td style={{ border: 0 }}>
+                    <IconButton
+                      style={{ padding: 0 }}
+                      aria-label="delete"
+                      onClick={() => {
+                        ondelete(i);
+                      }}
+                    >
+                      <DeleteIcon fontSize="small" />
+                    </IconButton>
+                  </td>
+                </tr>
+              );
+            })}
+            <tr>
+              <td colSpan={3} className="labour text-center">
+                Labour
+              </td>
+              <td className="text-center">{labour}</td>
+            </tr>
+            <tr>
+              <th colSpan={3} className="text-center">
+                Total
+              </th>
+              <th className="total">{getTotal()}</th>
+            </tr>
+          </table>
+          <p
+            className="my-5"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <Button
+              onClick={() => setShowBill(true)}
+              color="secondary"
+              variant="contained"
+            >
+              generate
+            </Button>
+          </p>
+        </div>
+      </div>
+      <Modal open={showBill}>
+        <div
           style={{
             display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
             justifyContent: "center",
           }}
         >
-          <Button
-            onClick={() => console.log(items)}
-            color="secondary"
-            variant="contained"
+          <PDFViewer
+            className="my-5"
+            style={{ width: "80%", height: "30em", margin: "auto" }}
           >
-            generate
+            <Bill
+              name={name}
+              date={date}
+              items={items}
+              total={getTotal()}
+              labour={labour}
+              image={image}
+            />
+          </PDFViewer>
+          <Button
+            style={{ width: 100 }}
+            variant="contained"
+            onClick={() => {
+              setShowBill(false);
+              clearAllBillDetail();
+            }}
+          >
+            close
           </Button>
-        </tr>
-      </div>
+        </div>
+      </Modal>
     </div>
   );
 }
--- src/Components/RowsInput.js
+++ src/Components/RowsInput.js
@@ -1,65 +1,14 @@
-import { TextField } from "@material-ui/core";
-import React, { useState } from "react";
+import React from "react";
 
 import "../App.css";
 
-export default function RowsInput({
-  items,
-  setItems,
-  index,
-  ondelete,
-  qa,
-  na,
-  ra,
-}) {
-  const [q, setQ] = useState(qa);
-  const [r, setR] = useState(items[index].rate);
-
+export default function RowsInput({ item }) {
   return (
     <>
-      <td className="description">
-        <TextField
-          type="text"
-          fullWidth
-          defaultValue={na}
-          onChange={(event) => {
-            let temp = items;
-            temp[index].name = event.target.value;
-            setItems(temp);
-          }}
-        />
-      </td>
-      <td className="rate">
-        <TextField
-          type="number"
-          fullWidth
-          placeholder={0}
-          defaultValue={items[index].rate}
-          inputProps={{ min: 0, style: { textAlign: "center" } }}
-          onChange={(event) => {
-            let temp = items;
-            temp[index].rate = parseInt(event.target.value);
-            setItems(temp);
-            setR(parseInt(event.target.value));
-          }}
-        />
-      </td>
-      <td className="quantity">
-        <TextField
-          type="number"
-          fullWidth
-          placeholder={0}
-          defaultValue={q}
-          inputProps={{ min: 0, style: { textAlign: "center" } }}
-          onChange={(event) => {
-            let temp = items;
-            temp[index].quantity = parseInt(event.target.value);
-            setItems(temp);
-            setQ(parseInt(event.target.value));
-          }}
-        />
-      </td>
-      <td className="total">{r * q}</td>
+      <td className="description">{item.name}</td>
+      <td className="rate">{item.rate}</td>
+      <td className="quantity">{item.quantity}</td>
+      <td className="total">{(item.rate * item.quantity).toFixed(2)}</td>
     </>
   );
 }
